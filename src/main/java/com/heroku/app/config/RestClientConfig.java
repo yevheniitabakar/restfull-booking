b430fc2f67f38ac91330827b27e873da24fe963c
ytabakar <ytabakar@blackberry.com>
2025-04-05 16:27:33 +0300

    Enhanced Singleton class with Lombok usages

--- src/main/java/com/heroku/app/config/RestClientConfig.java
+++ src/main/java/com/heroku/app/config/RestClientConfig.java
@@ -3,6 +3,7 @@ package com.heroku.app.config;
 import io.restassured.RestAssured;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
+import lombok.Getter;
 
 /**
  * Implements the Singleton design pattern to manage a single instance of the RestAssured configuration.
@@ -12,17 +13,12 @@ import io.restassured.filter.log.ResponseLoggingFilter;
  * consistency across all API calls in the test framework.
  */
 public class RestClientConfig {
-    private static RestClientConfig instance;
+    @Getter(lazy = true)
+    private static final RestClientConfig instance = new RestClientConfig();
 
+    // Private constructor to prevent instantiation
     private RestClientConfig() {
         RestAssured.baseURI = "https://restful-booker.herokuapp.com";
         RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
-    }
-
-    public static RestClientConfig getInstance() {
-        if (instance == null) {
-            instance = new RestClientConfig();
-        }
-        return instance;
     }
 }
